e6274743ad7acd713a23a5a7cb604dcb46d68b68
pablogodiaz <pablogodiaz+1@gmail.com>
2022-03-09 18:22:37 +0100

    Added compute function in Fibonacci.java

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,3 +1,11 @@
 public class Fibonacci {
-
+    public int compute(int i) {
+        if(i < 0) {
+            throw new RuntimeException("Number is negative");
+        } else if (i == 0 || i == 1) {
+            return i;
+        } else {
+            return compute(i-1) + compute(i - 2);
+        }
+    }
 }
