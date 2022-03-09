60ab2fb9644c8d4784729281a4b3946c72742a9f
pablogodiaz <pablogodiaz+1@gmail.com>
2022-03-09 18:24:35 +0100

    Added Fibonacci variable and @BeforeEach in FibonacciTest.java

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.BeforeEach;
+
 public class FibonacciTest {
 
+    public Fibonacci fibonacci;
+
+    @BeforeEach
+    public void init() {
+        fibonacci = new Fibonacci();
+    }
 }
